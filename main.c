452ab4714f2b3c20b03344c36288cfedec3597a5
eklompus <eklompus@student.21-school.ru>
2020-03-01 11:17:57 +0300

    test

--- main.c
+++ main.c
@@ -5,17 +5,21 @@
 #include <time.h>
 #include <unistd.h>
 
+#define SIZE (1000*1000*1000)
+#define MEMCPY ft_memcpy
+typedef unsigned long word;
+
 void *ft_memcpy(void *dst, const void *src, size_t n)
 {
 	void	*orig_dst;
 
 	orig_dst = dst;
-	while (n > sizeof(unsigned long))
+	while (n > sizeof(word))
 	{
-		*(unsigned long *)dst = *(unsigned long *)src;
-		dst = ((unsigned long *)dst) + 1;
-		src = ((unsigned long *)src) + 1;
-		n -= sizeof(unsigned long);
+		*(word *)dst = *(word *)src;
+		dst = ((word *)dst) + 1;
+		src = ((word *)src) + 1;
+		n -= sizeof(word);
 	}
 	while (n > 0)
 	{
@@ -27,13 +31,34 @@ void *ft_memcpy(void *dst, const void *src, size_t n)
 	return (orig_dst);
 }
 
-#define SIZE (1000*1000*1000)
-#define MEMCPY ft_memcpy
-
-int main()
+void test_memcpy(void *dst, void *src, size_t size)
 {
 	size_t	before;
 	size_t	after;
+
+	before = clock();
+	MEMCPY(dst, src, size);
+	after = clock();
+	printf("Both aligned: %zu, res: %d, word_size: %zu\n", after - before, memcmp(dst, src, size), sizeof(word));
+
+	before = clock();
+	MEMCPY(((unsigned char *)dst) + 3, src, size);
+	after = clock();
+	printf("Src aligned: %zu, res: %d, word_size: %zu\n", after - before, memcmp(((unsigned char *)dst) + 3, src, size), sizeof(word));
+
+	before = clock();
+	MEMCPY(dst, ((unsigned char *)src) + 3, size);
+	after = clock();
+	printf("Dst aligned: %zu, res: %d, word_size: %zu\n", after - before, memcmp(dst, ((unsigned char *)src) + 3, size), sizeof(word));
+
+	before = clock();
+	MEMCPY(((unsigned char *)dst) + 2, ((unsigned char *)src) + 3, size);
+	after = clock();
+	printf("Both non-aligned: %zu, res: %d, word_size: %zu\n", after - before, memcmp(((unsigned char *)dst) + 2, ((unsigned char *)src) + 3, size), sizeof(word));
+}
+
+int main()
+{
 	int 	fd;
 	void	*dst;
 	void	*src;
@@ -43,22 +68,7 @@ int main()
 	src = malloc(SIZE + 100);
 	read(fd, src, SIZE + 100);
 	close(fd);
-
-	before = clock();
-	MEMCPY(dst, src, SIZE);
-	after = clock();
-	printf("Both aligned: %zu, res: %d\n", after - before, memcmp(dst, src, SIZE));
-
-	before = clock();
-	MEMCPY(((unsigned char *)dst) + 3, src, SIZE);
-	after = clock();
-	printf("Src aligned: %zu, res: %d\n", after - before, memcmp(((unsigned char *)dst) + 3, src, SIZE));
-
-	before = clock();
-	MEMCPY(dst, ((unsigned char *)src) + 3, SIZE);
-	after = clock();
-	printf("Dst aligned: %zu, res: %d\n", after - before, memcmp(dst, ((unsigned char *)src) + 3, SIZE));
-
+	test_memcpy(dst, src, SIZE);
 	free(src);
 	free(dst);
 	return (0);
